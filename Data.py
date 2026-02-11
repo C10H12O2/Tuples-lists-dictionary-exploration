891ac36a4cb3d047c884115deb3d317c2a4a1f2a
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2026-02-11 10:24:11 +0300

    Printed the name, age and list of subjects for each student

--- Data.py
+++ Data.py
@@ -1,4 +1,7 @@
 student = [
     {"Name": "Eugene", "Age": 18, "City": "London", "Subjects": ["Math", "Physics", "English"]},
-    {"Name": "Joseph", "Age": 18, "City": "London", "Subjects": ["Math", "Biology", "Chemistry"]}
+    {"Name": "Joseph", "Age": 19, "City": "London", "Subjects": ["Math", "Biology", "Chemistry"]}
 ]
+
+for s in student:
+    print(f"{s['Name']} (Age {s['Age']}) studies: {', '.join(s['Subjects'])}")
