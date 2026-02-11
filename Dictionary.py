f5a5929324d990529e9283adabf7b60ec73b5c3d
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2026-02-11 10:14:11 +0300

    Deleted a key-value pair

--- Dictionary.py
+++ Dictionary.py
@@ -1,2 +1,6 @@
 student = {"Name": "Eugene", "Age": 18, "City": "London", "Gender": "Male"}
 student["Grade"] = "A"
+print ("Student Name:", student["Name"])
+
+del student["Age"]
+print ("Student Dictionary after deletion:", student)
