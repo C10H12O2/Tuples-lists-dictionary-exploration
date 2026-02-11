0756f7fa2ab39d5264674b333acd19ec40b8bd92
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2026-02-11 10:17:50 +0300

    Created a list of dictionaries

---
+++ Data.py
@@ -0,0 +1,4 @@
+student = [
+    {"Name": "Eugene", "Age": 18, "City": "London", "Subjects": ["Math", "Physics", "English"]},
+    {"Name": "Joseph", "Age": 18, "City": "London", "Subjects": ["Math", "Biology", "Chemistry"]}
+]
