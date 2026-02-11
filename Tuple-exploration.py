446eeb6bae6b6401b00c3d61616126f222de2913
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2026-02-11 09:59:04 +0300

    Created a new tuple bu concating the two tuples, one bing my_tuple and th eother being another_tuple

--- Tuple-exploration.py
+++ Tuple-exploration.py
@@ -1,2 +1,7 @@
 #Made a tuple with 4 elements
 my_tuple = (10, 20, 30, 40)
+print ("Original tuple:", my_tuple)
+
+anoter_tuple = (67, 89)
+result_tuple = my_tuple + anoter_tuple
+print("Concated tuple:", result_tuple)
